452b8da4564e9707ffc0ba071a4f112b6cb246ca
galen36123612 <82554245+galen36123612@users.noreply.github.com>
2025-03-19 15:40:02 +0800

    Update MessageList.tsx

--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -86,7 +86,7 @@ export default function MessageList({ messages }: MessageListProps) {
   );
 }*/
 
-/*import { Message } from "ai/react";
+import { Message } from "ai/react";
 import { useEffect, useRef, useState } from "react";
 
 interface MessageListProps {
@@ -148,31 +148,5 @@ export default function MessageList({ messages }: MessageListProps) {
       ))}
     </div>
   );
-}*/
-
-import { Message } from "ai/react";
-import { useEffect, useRef } from "react";
-interface MessageListProps {
-  messages: Message[];
 }
 
-export default function MessageList({ messages }: MessageListProps) {
-  const view = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    view.current!.scrollIntoView(false);
-  }, [messages]);
-
-  return (
-    <div ref={view} className="w-full border-none flex flex-col gap-1">
-      {messages.map((message: Message) => (
-        <div key={message.id} className="flex flex-row ">
-          {/* <span className="w-16 after:content-[':']">{message.role}</span> */}
-          <span className="flex-1 backdrop-blur-sm bg-white/10 rounded-md text-lg p-1 ">
-            {message.content}
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-}
